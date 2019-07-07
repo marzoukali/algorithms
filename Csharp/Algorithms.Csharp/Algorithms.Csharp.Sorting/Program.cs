fd0a7e94df7ea9bbb805c1df400293225745db37
ahmed mohamed marzouk <ahmedmarzouk.dh@gmail.com>
2019-07-08 00:53:46 +0100

    Add Selection Sort to C#

---
+++ Csharp/Algorithms.Csharp/Algorithms.Csharp.Sorting/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Csharp.Sorting
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 3, 8, 2, 0, 9, 5, 1, 8, 11, 7, 35, 1 };
+            SelectionSort.Sort(arr);
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Csharp/Algorithms.Csharp/Algorithms.Csharp.Sorting/SelectionSort.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Csharp.Sorting
+{
+    public static class SelectionSort
+    {
+        public static void Sort(int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                int minIndex = i;
+
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr[minIndex])
+                    {
+                        minIndex = j;
+                    }
+
+                    Swap(ref arr[i], ref arr[minIndex]);
+                }
+
+            }
+        }
+
+
+        private static void Swap(ref int x, ref int y)
+        {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+
+    }
+}
